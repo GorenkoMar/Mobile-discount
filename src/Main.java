f71011dfe873b566a5fde4771925ac51672d73af
Gorenko Maria <kirillovsub@yandex.ru>
2024-11-13 11:42:46 +0300

    first commit

--- src/Main.java
+++ src/Main.java
@@ -2,15 +2,15 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        int customer_account = 750;
-        int deposit_amount = 1250;
+        int customerAccount = 750;
+        int depositAmount = 1250;
         int bonus;
-        if (deposit_amount >= 1000) {
-            bonus = deposit_amount / 100;
+        if (depositAmount >= 1000) {
+            bonus = depositAmount / 100;
         } else {
             bonus = 0;
         }
-        int Account_total = deposit_amount + customer_account + bonus;
+        int Account_total = depositAmount + customerAccount + bonus;
         System.out.printf("На Вашем счету: " + Account_total);
         System.out.printf("     Начислено бонусов: " + bonus);
 
