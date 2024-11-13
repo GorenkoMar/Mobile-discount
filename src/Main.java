1cce85c7d17ec5816733c169c4d2b381b4eb1db4
Gorenko Maria <kirillovsub@yandex.ru>
2024-11-13 12:08:26 +0300

    first commit

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,19 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-        int customerAccount = 750;
-        int depositAmount = 1250;
-        int bonus;
-        if (depositAmount >= 1000) {
-            bonus = depositAmount / 100;
-        } else {
-            bonus = 0;
-        }
-        int Account_total = depositAmount + customerAccount + bonus;
-        System.out.printf("На Вашем счету: " + Account_total);
-        System.out.printf("     Начислено бонусов: " + bonus);
 
+  public static void main(String[] args) {
+    int customerAccount = 750;
+    int depositAmount = 1250;
+    int bonus;
+    if (depositAmount >= 1000) {
+      bonus = depositAmount / 100;
+    } else {
+      bonus = 0;
     }
+    int accountTotal = depositAmount + customerAccount + bonus;
+    System.out.printf("На Вашем счету: " + accountTotal);
+    System.out.printf("     Начислено бонусов: " + bonus);
+
+  }
 }
